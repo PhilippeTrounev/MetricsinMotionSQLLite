76f92ea42cde05b5cc9cd85f13ef18111dabbce8
PhilippeTrounev <ptrounev@gmail.com>
2013-10-21 23:08:05 +0200

    New
    Changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MetricsinMotionSQLLite::Application.config.secret_token = '40efff07a81e78644004b72cd4bb60d6d00019cf5efe37e36603104f328ff57f8ba9b87851f8f9527eb306007e53a77ec6fc3a8d2be1126c80d13f526aee949b'
